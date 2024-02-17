03a151c1fda4dd2af35ad266aeff009df5fc98b0
sharif ahmad <sharif.ahmad@agoda.com>
2024-02-17 23:23:10 +0700

    change main file and import correct packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,14 @@
 plugins {
     kotlin("jvm") version "1.9.22"
     application
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.0"
 }
 
 group = "me.sahmad"
 version = "1.0-SNAPSHOT"
 
 application {
-    mainClass = "me.sahmad.ApplicationKt"
+    mainClass = "me.sahmad.webrtcsignalingserver.ApplicationKt"
 }
 
 repositories {
@@ -16,8 +17,9 @@ repositories {
 
 dependencies {
     implementation("io.ktor:ktor-server-netty:2.3.8")
-    implementation("io.ktor:ktor-html-builder:2.3.8")
-    implementation("io.ktor:ktor-websockets:2.3.8")
+    implementation("io.ktor:ktor-server-content-negotiation:2.3.8")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.8")
+    implementation("io.ktor:ktor-server-websockets:2.3.8")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 }
